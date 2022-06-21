dd40aef791644df372f5aec144bf52bee65f3fd4
adab786 <siddiquiadab@eng.rizvi.edu.in>
2022-06-22 00:37:27 +0530

    card added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import reactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Card(props) {
+  <h1>My Contacts</h1>;
+  return (
+    <div>
+      <h2> {props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+reactDom.render(
+  <div>
+    <h1>MY CONTACT </h1>
+    <Card
+      name="MONA lisa"
+      img="https://ih1.redbubble.net/image.694518903.3043/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+
+    <Card
+      name="computers"
+      img="https://images.freecreatives.com/wp-content/uploads/2015/03/Huge-Backgrounds-63.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
